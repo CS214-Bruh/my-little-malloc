db41f5812f30f5c7b17cd1d75d515a35f2331548
Ivan Zheng <71520179+IvanZ505@users.noreply.github.com>
2024-02-19 18:05:13 -0500

    Update mytest.c

--- mytest.c
+++ mytest.c
@@ -1,244 +1,241 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/time.h>
-
-// Compile with -DREALMALLOC to use the real malloc() instead of mymalloc()
-#ifndef REALMALLOC
+#include <time.h>
 #include "mymalloc.h"
-#endif
 
-#define BLOCKSIZE 512
-#define MEMSIZE 4096
-#define HEADERSIZE 8
-
-#define NUMMALTESTS 4
-unsigned char *arr[BLOCKSIZE/2];
-
-// Standard malloc and free test on 1 huge block.
-int standard_malloc_free() {
-    int correct = 1;
-    // Malloc it
-    int* pointer = malloc(sizeof(char) * (MEMSIZE-HEADERSIZE-1));
-
-    // Checking this
-    for(int i = 0; i < BLOCKSIZE/2; i++) {
-        *(pointer+i) = i;
-    }
-    for(int i = 0; i < BLOCKSIZE/2; i++) {
-        if (*(pointer+i) != i) correct = 0;
-    }
-    // Free it
-    free(pointer);
-    // Malloc again
-    int* pointer2 = malloc(sizeof(char) * (MEMSIZE-HEADERSIZE-1));
-    for(int i = 0; i < BLOCKSIZE/2; i++) {
-        *(pointer2+i) = i;
-
-    }
-    for(int i = 0; i < BLOCKSIZE/2; i++) {
-        if (*(pointer2+i) != i) correct = 0;
-    }
-    free(pointer2);
-    return correct;
-}
-
-// Malloc Error (Too large of a Block)
-int malloc_huge() {
-    int* pointer = malloc(sizeof(char) * (MEMSIZE-HEADERSIZE-1));
-    int errors = 0;
-    for(int i = 0; i < BLOCKSIZE/2; i++) {
-        *(pointer+i) = i;
-    }
-    for(int i = 0; i < BLOCKSIZE/2; i++) {
-        if (*(pointer+i) != i) errors++;
-    }
-    free(pointer);
-    return errors;
-}
-
-// Malloc 1 less than size of array and try to malloc 1 more to see if it works.
-void malloc_huge_minus_one() {
-    char *pointer = malloc(sizeof(char)* (MEMSIZE-HEADERSIZE-1)-8);
-    char *pointer2 = malloc(sizeof(char));
-}
-
-// Malloc an entire array of chars and count to make sure the all the spaces have been used
-// All the bytes for headers and payload should add up to the MEMSIZE
-int malloc_max_number() {
-    // malloc each and every entry to the array.
-    for(int i = 0; i < BLOCKSIZE/2; i++) {
-        arr[i] = malloc(sizeof(char));
-    }
+#define NANOSECONDS 1000000000.0
+#define NUMTESTS 50 // Indicates how many times we want to stress test our code, set as desired
 
 
-    // Give each one a unique number
-    for(int i = 0; i < BLOCKSIZE/2; i++) {
-        *arr[i] = i;
-    }
-    // Check that each one matches.
-    int errors = 0;
-    for(int i = 0; i < BLOCKSIZE/2; i++) {
-        if(i != *arr[i]) errors++;
-    }
-    return errors; 
-}
+// THE FOLLOWING SECTION IS ALL CODE FOR DEBUGGING PURPOSES
+// ---------------------------------------------------------
 
-void free_max_number() {
-    for(int i = 0; i < BLOCKSIZE/2; i++) {
-        free(arr[i]);
-    }
-}
 
-// Free 2 adjacent memory addresses to see if coalesce will work.
-int free_coalesce_adj_test() {
-    malloc_max_number();
-    // Free up only half of the
-    for(int i = 0; i < BLOCKSIZE/2; i+=2) {
-        free(arr[i]);
-    } 
+// Uncomment following line for debugging purposes
 
-    // Free the other half now
-    for(int i = 1; i < BLOCKSIZE/2; i+=2) {
-        free(arr[i]);
-    }
-    int correct = 1;
-    // They should coalesce and I should be able to allocate another 4088 bytes of data
-    unsigned char* new_ptr = malloc(sizeof(char) * ((MEMSIZE-HEADERSIZE-1)));
-    for(int i = 0; i < BLOCKSIZE/2; i++) {
-        *(new_ptr+i) = i;
+// #define DEBUG 1
 
-    }
-    for(int i = 0; i < BLOCKSIZE/2; i++) {
-//        printf("%d\n",*(new_ptr + i));
-        if (*(new_ptr + i) != i) correct = 0;
-    }
-    free(new_ptr);
-    return correct;
-}
-
-// Speed test for malloc() and free() as well as the combination of them.
-struct timeval malloc_start, malloc_stop, free_start, free_stop;
-
-void speed_test(int number_of_times) {
-    // Just run speed test for malloc
-    int i = 0;
-    // Keep track of some stats for malloc
-    double malloc_avg = 0;
-    double malloc_top = 0;
-    double malloc_low = 0;
-    // Some for free
-    double free_avg = 0;
-    double free_top = 0;
-    double free_low = 0;
-
-    while(i < number_of_times) {
-        gettimeofday(&malloc_start, NULL);
-        malloc_max_number();
-        gettimeofday(&malloc_stop, NULL);
-        double malloc_time = ((malloc_stop.tv_sec - malloc_start.tv_sec) * 1000000 + malloc_stop.tv_usec - malloc_start.tv_usec)* 0.001;
-        if(malloc_time > malloc_top) malloc_top = malloc_time;
-        if(malloc_time < malloc_low) malloc_low = malloc_time;
-        malloc_avg += malloc_time;
-        // printf("Malloc-ing %d objects took %lf ms\n", BLOCKSIZE/2, malloc_time);
-        
-        // Run code for freeing... and add the same speed test format except for free...
-        gettimeofday(&free_start, NULL);
-        free_max_number();
-        gettimeofday(&free_stop, NULL);
-        double free_time = ((free_stop.tv_sec - free_start.tv_sec) * 1000000 + free_stop.tv_usec - free_start.tv_usec)* 0.001;
-        if(free_time > free_top) free_top = free_time;
-        if(free_time < free_low) free_low = free_time;
-        free_avg += free_time;
-        //printf("Freeing %d objects took %lf ms\n", BLOCKSIZE/2,free_time);
-
-        i++;
-    }
-    printf("Malloc Statistics:\n");
-    printf("Total Items Malloc-ed: %d\n", (BLOCKSIZE/2)*number_of_times);
-    printf("Average Malloc-ing Time per Run of %d objects: %lf\n", BLOCKSIZE/2, malloc_avg/number_of_times);
-    printf("Slowest Run of %d objects: %lf\n", BLOCKSIZE/2, malloc_top);
-    printf("Fastest Run of %d objects: %lf\n", BLOCKSIZE/2, malloc_low);
-
-    printf("Free Statistics:\n");
-    printf("Total Items Freed: %d\n", (BLOCKSIZE/2)*number_of_times);
-    printf("Average Freeing Time per Run of %d objects: %lf\n", BLOCKSIZE/2, free_avg/number_of_times);
-    printf("Slowest Run of %d objects: %lf\n", BLOCKSIZE/2, free_top);
-    printf("Fastest Run of %d objects: %lf\n", BLOCKSIZE/2, free_low);
-
-}
-
-int my_coalesce_test() {
-    // Malloc it
-    char* pointer = malloc((sizeof(char) * (MEMSIZE-HEADERSIZE)-1)/4);
-    char* pointer2 = malloc((sizeof(char) * (MEMSIZE-HEADERSIZE)-1)/4);
-    char* pointer3 = malloc((sizeof(char) * (MEMSIZE-HEADERSIZE)-1)/4);
-    // Free it
-    free(pointer);
-    free(pointer3);
-    free(pointer2);
-    // Malloc again
-   unsigned char* pointer4 = malloc(sizeof(char) * (MEMSIZE-HEADERSIZE)-1);
-    for(int i = 0; i < BLOCKSIZE; i++) {
-        *(pointer4+i) = i%256;
-    }
-    // Check for errors
-    int correct = 1;
-    for(int i = 0; i < BLOCKSIZE; i++) {
-        if(*(pointer4+i) != i%256) correct = 0;
-        
-    }
-    free(pointer4);
-    return correct;
-}
-
-int main(int argc, char **argv)
+// Helper method for debugging TEST 3
+#ifdef DEBUG
+void printIndexAllocated(int i)
 {
-//    double* a = malloc(sizeof(double));
-//    char* b = malloc(sizeof(char)*7);
-//   malloc_max_number();
-//    malloc_huge_minus_one();
-    // printf("Number of Total Errors: %d\n", malloc_max_number());
-    //speed_test(0);
-    //printf("Number of Total errors for coalesce: %d\n", my_coalesce_test());
-    //free_coalesce_adj_test();
-    char* test_funcs[] = {"Full Malloc and Free Test", "Speed Testing", "Error Testing"};
+    printf("indexAllocated = %d\n", i);
+};
+#endif
+// ---------------------------------------------------------
 
-    int num_of_tests = 3;
-    printf("Welcome to the testing environment for mymalloc...\n");
-    printf("Please choose from the following test functions:\n");
-    for(int i =0; i < num_of_tests; i++) {
-        printf("%d: %s\n", i+1, test_funcs[i]);
+
+/// @brief Prints and returns average time taken over "size" iterations
+/// @param arrTest Array of test runtimes
+/// @param size Size of arrTest
+/// @param stressTestNumber Test number to be printed 
+/// @return Average of arrTest 
+
+double printAvgTest(double *arrTest, int size, int stressTestNumber)
+{
+    double runningSum = 0;
+    for (int i = 0; i < size; i++)
+    {
+        runningSum += arrTest[i];
     }
-    int choice;
-    scanf("%d", &choice);
-    switch(choice) {
-        case(1): {
-            int passed_subtests = 0;
-            printf("Running Standard malloc and free test...");
-            if(standard_malloc_free() == 1) {printf("Passed\n");passed_subtests++;}else{printf("Failed\n");}
-            printf("Allocating Maximum Space Possible...");
-            int mal_huge_errors = malloc_huge();
-            printf("%d errors\n", mal_huge_errors);
-            if(mal_huge_errors == 0) passed_subtests++;
-            printf("Running Basic Coalescing Test...");
-            if(my_coalesce_test()) {printf("Passed\n");passed_subtests++;}else{ printf("Failed");}
-            printf("Running Adjacent Free Test to test for coalescing...");
-            if(free_coalesce_adj_test()) { printf("Passed\n"); passed_subtests++;}else{ printf("Failed\n");}
-            printf("\nTotal malloc free tests Passed: %d/%d\n", passed_subtests, NUMMALTESTS);
-            break;
-        }
-        case(2): {
-            int num_of_tests;
-            printf("How many Tests?");
-            scanf("%d", &num_of_tests);
-            speed_test(num_of_tests);
-            break;
-        }
+    double totalSum = runningSum / ((double)size);
+    printf("AVG RUNTIME of TEST %d over %d iterations: %.10lf seconds\n", stressTestNumber, size, totalSum);
+    return totalSum;
+}
 
+int main()
+{
+    srand(time(NULL));
+
+    struct timespec startTime;
+    struct timespec endTime;
+
+    double testRuntimes[NUMTESTS];
+
+    for (int testNo = 0; testNo < NUMTESTS; testNo++)
+    {
+        timespec_get(&startTime, TIME_UTC);
+        for (int i = 0; i < 120; i++)
+        {
+            void *testptr = malloc(1);
+            free(testptr);
+        }
+        timespec_get(&endTime, TIME_UTC);
+        testRuntimes[testNo] = (endTime.tv_nsec - startTime.tv_nsec) / NANOSECONDS;
     }
 
+    printAvgTest(testRuntimes, NUMTESTS, 1);
 
-    return EXIT_SUCCESS;
+    // TEST TWO
+    for (int testNo = 0; testNo < NUMTESTS; testNo++)
+    {
+        timespec_get(&startTime, TIME_UTC);
+        void **ptrTestTwo = (void **)malloc((sizeof(void *) * 120));
+
+        for (int j = 0; j < 100; j++)
+        {
+            for (int i = 0; i < 120; i++)
+            {
+                ptrTestTwo[i] = malloc(1);
+            }
+            for (int i = 0; i < 120; i++)
+            {
+                free(ptrTestTwo[i]);
+            }
+        }
+
+        free(ptrTestTwo);
+        timespec_get(&endTime, TIME_UTC);
+        testRuntimes[testNo] = (endTime.tv_nsec - startTime.tv_nsec) / NANOSECONDS;
+    }
+
+    printAvgTest(testRuntimes, NUMTESTS, 2);
+
+    // TEST THREE
+    for (int testNo = 0; testNo < NUMTESTS; testNo++)
+    {
+        timespec_get(&startTime, TIME_UTC);
+
+        int allocateCounter = 0;
+        int indexAllocated = 0;
+        void **ptrTestThree = (void **)malloc(sizeof(void *) * 120);
+
+        while (allocateCounter < 120)
+        {
+            int r = rand();
+            if (r % 2 == 0)
+            {
+                if (indexAllocated < 0)
+                    indexAllocated = 0;
+                ptrTestThree[indexAllocated] = malloc(1);
+                allocateCounter++;
+                indexAllocated++;
+#ifdef DEBUG
+                printIndexAllocated();
+#endif
+            }
+            else
+            {
+                if (indexAllocated > 0)
+                {
+                    free(ptrTestThree[indexAllocated - 1]);
+                    indexAllocated--;
+#ifdef DEBUG
+                    printIndexAllocated();
+#endif
+                }
+#ifdef DEBUG
+                else
+                {
+                    printf("Nothing in array\n");
+                }
+#endif
+            }
+        }
+
+        for (int i = 0; i < indexAllocated; i++)
+        {
+            free(ptrTestThree[i]);
+#ifdef DEBUG
+            printf("SUCCESSFUL DEALLOCATION AT LOC %d\n", i);
+#endif
+        }
+
+        free(ptrTestThree);
+        timespec_get(&endTime, TIME_UTC);
+        testRuntimes[testNo] = (endTime.tv_nsec - startTime.tv_nsec) / NANOSECONDS;
+    }
+
+    printAvgTest(testRuntimes,NUMTESTS,3); 
+
+    // TEST 4
+    // Description of Test 4: We will be testing the coalescing
+    // abilities of our code, by allocating a contiguous array
+    // of pointers, then free'ing only the odd-numbered pointers
+    // leaving a fragmented array that will coalesce when we free
+    // the even-numbered pointers
+    for (int testNo = 0; testNo < NUMTESTS; testNo++)
+    {
+        timespec_get(&startTime, TIME_UTC);
+
+        void **ptrTestFour = (void **)malloc(sizeof(void *) * 120);
+        for (int i = 0; i < 120; i++)
+        {
+            ptrTestFour[i] = malloc(1);
+        }
+        for (int i = 0; i < 120; i += 2)
+        {
+            free(ptrTestFour[i]);
+        }
+        for (int i = 1; i < 120; i += 2)
+        {
+            free(ptrTestFour[i]);
+        }
+        free(ptrTestFour);
+        timespec_get(&endTime, TIME_UTC);
+        testRuntimes[testNo] = (endTime.tv_nsec - startTime.tv_nsec) / NANOSECONDS;
+    }
+
+    printAvgTest(testRuntimes, NUMTESTS, 4);
+
+    // TEST 5: SEE README.TXT FOR FULL DESCRIPTION
+    // We will create an array of pointers that will be free'd
+    // from both ends, coalescing into larger and larger blocks at each
+    // endpt, until both merge into one. Which one is faster? Let's find
+    // out!
+
+    printf("---\nSee source code for explanation of Test 5\n---\n"); 
+    double testRuntimesLeft[NUMTESTS];
+    double testRuntimesRight[NUMTESTS];
+    for (int testNo = 0; testNo < NUMTESTS; testNo++)
+
+    {
+        struct timespec startTimeLeft, startTimeRight, endTimeLeft, endTimeRight;
+
+        int sizeOfList = 120;
+
+        timespec_get(&startTime, TIME_UTC);
+
+        void **ptrTestFive = (void **)malloc(sizeof(void *) * 120);
+        for (int i = 0; i < sizeOfList; i++)
+        {
+            ptrTestFive[i] = malloc(1);
+        }
+        timespec_get(&startTimeLeft, TIME_UTC);
+
+        for (int i = 0; i < sizeOfList / 2; i++)
+        {
+            free(ptrTestFive[i]);
+        }
+        timespec_get(&endTimeLeft, TIME_UTC);
+        timespec_get(&startTimeRight, TIME_UTC);
+
+        for (int i = sizeOfList - 1; i > sizeOfList / 2 - 1; i--)
+        {
+            free(ptrTestFive[i]);
+        }
+
+        free(ptrTestFive);
+
+        timespec_get(&endTimeRight, TIME_UTC);
+        timespec_get(&endTime, TIME_UTC);
+        testRuntimes[testNo] = (endTime.tv_nsec - startTime.tv_nsec) / NANOSECONDS;
+        testRuntimesLeft[testNo] = (endTimeLeft.tv_nsec - startTimeLeft.tv_nsec) / NANOSECONDS;
+        testRuntimesRight[testNo] = (endTimeRight.tv_nsec - startTimeRight.tv_nsec) / NANOSECONDS;
+    }
+    printf("LEFT    ");
+    printAvgTest(testRuntimesLeft, NUMTESTS, 5);
+    printf("RIGHT   ");
+    printAvgTest(testRuntimesRight, NUMTESTS, 5);
+    printf("OVERALL ");
+    printAvgTest(testRuntimes, NUMTESTS, 5);
+
+    // If we run this program, indeed, we'll find that the right
+    // average runtime is an order of magnitude larger than the
+    // left average runtime, since coalescing on the left direction
+    // doesn't require traversing the entire list to coalesce,
+    // whereas the right hand version does.
+
+    memLeak();
+    return 1;
 }
